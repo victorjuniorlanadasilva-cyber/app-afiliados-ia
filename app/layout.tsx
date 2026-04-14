641ce809da67fdb14de8ad33003de2288a4cf18c
Victor <victorjuniorlanadasilva@gmail.com>
2026-04-14 10:46:14 -0300

    fix layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,19 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
+import type { Metadata } from "next";
 
-const inter = Inter({
-  subsets: ["latin"],
-});
-
-export const metadata = {
+export const metadata: Metadata = {
   title: "DarianIA",
   description: "Sua IA para encontrar produtos e economizar.",
-  openGraph: {
-    title: "DarianIA",
-    description: "Sua IA para encontrar produtos e economizar.",
-    url: "https://app-darian-ia.vercel.app",
-    siteName: "DarianIA",
-    images: [
-      {
-        url: "/og.png",
-        width: 1200,
-        height: 630,
-      },
-    ],
-    type: "website",
-  },
 };
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="pt-br">
+      <body>{children}</body>
+    </html>
+  );
+}
